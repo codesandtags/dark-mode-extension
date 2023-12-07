657d99327dbbb7b62a32177024bb9b21ca7529b9
codesandtags <codesandtags@gmail.com>
2023-12-06 20:56:52 -0500

    feat: added gp-pages to support website

---
+++ website/main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import appLogo from "/logo.png";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <a href="https://github.com/codesandtags/dark-mode-extension" target="_blank">
+      <img src="${appLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1>Dark Mode Enabler!</h1>
+    <p class="read-the-docs">
+      Click on the logo to learn more.
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
